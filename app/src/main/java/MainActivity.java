945fafb0ef4e199879e5200a68c72bdc308a8199
yugandhar <yugandhardcs21@gmail.com>
2016-02-26 15:36:25 +0530

    Initial commit

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Kiran on 29-10-2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.kiran.sample.R;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo);
+    }
+}
